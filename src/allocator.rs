71a02fd61c961569b79276aa1e5d2d29a9476827
Sam <samuelgardner101@gmail.com>
2024-02-06 17:18:34 +0000

    Bump Allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,14 +1,14 @@
-use alloc::alloc::{GlobalAlloc, Layout};
-use core::ptr::null_mut;
+use alloc::alloc::{GlobalAlloc};
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::{VirtAddr};
-use linked_list_allocator::LockedHeap;
+use crate::allocator::bump::BumpAllocator;
 
+pub mod bump;
 
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
 
 pub const HEAP_START: usize = 0x4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
@@ -35,4 +35,25 @@ pub fn init_heap(
 
     unsafe { ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE); }
     Ok(())
+}
+
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl <A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked { inner: spin::Mutex::new(inner) }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+
+// Align a given "addr" upwards to the specified "align" value
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
